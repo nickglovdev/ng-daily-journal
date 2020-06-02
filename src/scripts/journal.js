3f99c8857c5c1e1209ce6a3d9e2b432d527e266b
nickglovdev <nickglovdev@gmail.com>
2020-06-02 15:32:23 -0500

    Added three new objects

---
+++ src/scripts/journal.js
@@ -0,0 +1,21 @@
+// Is this what it is asking?
+const journalEntry = {
+    date:"5-20-20",
+    conceptsCovered:"HTML",
+    journalEntry:"It's fun to relearn more HTML",
+    mood:"Happy"
+}
+
+const journalEntry = {
+    date:"5-22-20",
+    conceptsCovered:"CSS",
+    journalEntry:"I love to play around with CSS",
+    mood:"Happy"
+}
+
+const journalEntry = {
+    date:"5-29-20",
+    conceptsCovered:"Objects",
+    journalEntry:"It was good to look over Objects again",
+    mood:"Happy"
+}
