cb112e5dda808cd29420c40b352025eb5d313b80
nickglovdev <nickglovdev@gmail.com>
2020-06-21 11:04:25 -0500

    Added click event with values attached to the inputs

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -3,3 +3,18 @@ import renderJournalEntries from './entryList.js';
 
 API.getJournalEntries().then
 ((entriesResponse) => renderJournalEntries(entriesResponse))
+
+const buttonSelectionSubmit = document.querySelector(".record-entry")
+
+buttonSelectionSubmit.addEventListener("click", clickEvent => {
+    const dateSelection = document.querySelector("#journalDate").value
+    const conceptSelection = document.querySelector("#journalConcept").value
+    const entrySelection = document.querySelector("#journalEntry").value
+    const moodSelection = document.querySelector("#journalMood").vaue
+
+    if (dateSelection === "" || conceptSelection === "" || entrySelection === "" || moodSelection === "") {
+        alert("Missing Input")
+    }
+
+    
+})
